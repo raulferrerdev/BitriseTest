2205d5241a7dfadc5033d448a34dee2a9270fdef
raulferrerdev <raul.ferrer.dev@gmail.com>
2020-04-27 08:38:56 +0200

    Added simple tests.

---
+++ BitriseTest/BitriseModel.swift
@@ -0,0 +1,15 @@
+//
+//  BitriseModel.swift
+//  BitriseTest
+//
+//  Created by RaulF on 27/04/2020.
+//  Copyright © 2020 ImTech. All rights reserved.
+//
+
+import Foundation
+
+struct BitriseModel {
+    var username: String
+    var repository: String
+    var numberOfProjects: Int
+}
--- BitriseTestTests/BitriseTestTests.swift
+++ BitriseTestTests/BitriseTestTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import BitriseTest
 
 class BitriseTestTests: XCTestCase {
+    
+    let model = BitriseModel(username: "user", repository: "github", numberOfProjects: 20)
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +21,11 @@ class BitriseTestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testModelContainsAUsername() {
+        XCTAssertTrue(!model.username.isEmpty)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testModelUsernameIsCorrect() {
+        XCTAssertEqual(model.username, "user")
     }
-
 }
